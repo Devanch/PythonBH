c130849c60c6487ce0c366b4bf7b8baf2208b65a
pavel <pavelanyr@gmail.com>
2021-11-22 18:21:20 +0300

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,26 @@
+# Получение чисел
+# a = float(input()); 
+# b = float(input()); 
+# c = float(input()); 
+
+# Тестовые данные
+a = 3;
+b = 2;
+c = 1;
+print(f"Вы вели следующие числа: {a},{b},{c} ")
+
+# Применение формулы
+f = float(a**2 + b**2);
+g = float(3*b - 4);
+h = float(4 * c**5 / 6);
+
+res = f / g ;
+resa = res  /  h;
+
+# Вывод результатов
+print(resa);
+
+print("Целая часть " + str(res  //  h))
+print("Остаток " + str(res  %  h))
+print("res: " + str(res))
+print("h: " + str(h))
