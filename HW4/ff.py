cbddf25a6f14eebc45db9828d367e3fd29fe49eb
pavel <pavelanyr@gmail.com>
2021-12-17 23:19:20 +0300

    WOW

---
+++ HW4/ff.py
@@ -0,0 +1,10 @@
+from pymongo import MongoClient
+
+# Create the client
+client = MongoClient('localhost', 27017)
+
+# Connect to our database
+db = client['SeriesDB']
+
+# Fetch our series collection
+series_collection = db['series']
