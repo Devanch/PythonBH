db18a876d1eb4b8f21be5189f290eb7e8986a6b4
Pavel <48050927+Devanch@users.noreply.github.com>
2022-01-14 16:29:50 +0300

    fix

--- HW4/main.py
+++ HW4/main.py
@@ -6,47 +6,75 @@ class Point:
     y = 0.0
 
 class Line:
-    point_a = Point() 
-    point_b = Point() 
-    def len(self):
-        return  sqrt((self.point_b.x - self.point_a.x)**2 + (self.point_b.y - self.point_a.y)**2)
+    pnt_a = Point()
+    pnt_b = Point()
+    
+    def setLine(self, x1, y1, x2, y2):
+        self.pnt_a.x = x1
+        self.pnt_a.y = y1
+        self.pnt_b.x = x2
+        self.pnt_b.y = y2
+
+    def length(self):
+         return sqrt((self.pnt_b.x - self.pnt_a.x)**2 + (self.pnt_b.y - self.pnt_a.y)**2)
+
+class Shape(ABC):
+    
+    @abstractmethod     
+    def area(self):
+        pass
+    
+    @abstractmethod     
+    def perimeter(self):
+        pass
 
 
-class Shape(ABC): 
+class Square(Line, Shape):
     @property
     def area(self):
+        self._area = super().length()**2
         return self._area
 
     @property
     def perimeter(self):
+        self._perimeter = super().length()*4
         return self._perimeter
 
-class Square(Line, Shape):
-    def fun(self):
-        return super().len() * 4
-        self.perimeter = 10
 
-    area = 20
+class Rect(Shape):
+    diag = Line()
+    
+    @property
+    def perimeter(self):
+        return abs(self.diag.pnt_a.x - self.diag.pnt_b.x)*2 + abs(self.diag.pnt_a.y - self.diag.pnt_b.y)*2
+    
+    @property    
+    def area(self):
+        return abs(self.diag.pnt_a.x - self.diag.pnt_b.x) * abs(self.diag.pnt_a.y - self.diag.pnt_b.y)
 
-class React(Shape):
-    line = Line()
 
 class Cube(Square):
-    volume = 0;
+    def volume(self):
+        return super().area  * super().length()
 
 ln = Line()
-ln.point_a.x = 5
-ln.point_a.y = 4
-ln.point_b.x = 9
-ln.point_b.y = 1
-print(f"Line Length:  {ln.len()}")
+ln.setLine(5, 4, 9, 1)
+print(f"Line Length:  {ln.length()}")
+print(f"-"*20)
 
 sq = Square()
-sq.point_a.x = 5
-sq.point_a.y = 4
-sq.point_b.x = 9
-sq.point_b.y = 1
-print(f"Square Length: {sq.len()}")
+sq.setLine(5, 4, 9, 1)
+print(f"Square Length: {sq.length()}")
 print(f"Square Perimetr: {sq.perimeter}")
 print(f"Square Area: {sq.area}")
-print(f"Square fun: {sq.fun()}")
+print(f"-"*20)
+
+rt = Rect()
+rt.diag.setLine(1, 1, 4, 4)
+print(f"Rect Perimetr: {rt.perimeter}")
+print(f"Rect Area: {rt.area}")
+print(f"-"*20)
+
+cb = Cube()
+cb.setLine(5, 4, 9, 1)
+print(f"Cube Volume: {cb.volume()}")
