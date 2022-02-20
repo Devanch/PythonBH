6fb1bd4be348c4a6e6026ceb69e5da3c7c3194a0
Pavel <48050927+Devanch@users.noreply.github.com>
2022-02-20 13:44:41 +0300

    Update newyeartoys.py

--- HW6/newyeartoys.py
+++ HW6/newyeartoys.py
@@ -1,5 +1,6 @@
 import random
 import click
+from enum import IntEnum
 
 
 @click.group()
@@ -7,12 +8,32 @@ def cli():
     pass
 
 
+class Colors(IntEnum):
+    Red = 1
+    Orange = 2
+    Yellow = 3
+    Green = 4
+    Blue = 5
+    Purple = 6
+
+
+class Toys(IntEnum):
+    angel = 1
+    elk = 2
+    boot = 3
+    balloon = 4
+    garland = 5
+
+
 @click.command()
 @click.option('--toys')
 def toys(toys):
-    colors = ('Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Purple')
-    toys = ('angel', 'elk', 'boot', 'balloon', 'garland')
-    res = "Random christmas toys - " + random.choice(colors) + " " + random.choice(toys)
+    colors = (1, 2, 3, 4, 5, 6)
+    toys = (1, 2, 3, 4, 5)
+    a = random.choice(toys)
+    b = random.choice(colors)
+    res = str("Random christmas toys - " + Colors(b).name + " " + Toys(a).name)
+    # click.echo(Toys(a).name)
     click.echo(res)
 
 
